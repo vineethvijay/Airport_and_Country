10ffecf84ae5af66585fc0fd838d473a7b7706fb
vineethvijay <vineeth@innovatise.com>
2018-06-20 21:30:17 +0530

    modified deploy-script

--- deploy-airport.sh
+++ deploy-airport.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mv airport/run.war airports-assembly-1.0.1.jar
-cp airport/airports-assembly-1.1.0.jar run.war
 
-docker-compose -d restart
+mv airport/run.jar airport/airports-assembly-1.0.1.jar
+cp airport/airports-assembly-1.1.0.jar airport/run.jar
+#bash clean.sh
+
+docker-compose up --build
 
