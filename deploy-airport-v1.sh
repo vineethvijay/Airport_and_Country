33f5b3a4dc0fb1914918b8e3dc4422ce1369b41d
vineethvijay <vineeth@innovatise.com>
2018-06-21 16:18:30 +0530

    airport version-2 deploy fail-over script added

---
+++ deploy-airport-v1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+deploy_to_container=$1
+
+if [ -n "$1" ]
+then
+  echo -e "\nDeploying version-2 to $deploy_to_container "
+else
+  echo -e "Script needs deploy_to_container name - (airports1 / airports2) \n..Exiting..\n"
+  exit
+fi
+
+if [ $deploy_to_container == "airports2" ]
+then
+	docker-compose stop airports2
+	cp -f deploy/airports-assembly-1.0.1.jar airport2/run.jar
+	rm airport2/RUNNING_PID
+	docker-compose start airports2
+elif [ $deploy_to_container == "airports1" ]
+then
+	docker-compose stop airports1
+	cp -f deploy/airports-assembly-1.0.1.jar airport1/run.jar
+	rm airport1/RUNNING_PID
+	docker-compose start airports1
+else
+	echo "No matching container name"
+
+fi
+
+
--- deploy-airport.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-mv airport/run.jar airport/airports-assembly-1.0.1.jar
-cp airport/airports-assembly-1.1.0.jar airport/run.jar
-#bash clean.sh
-
-docker-compose up --build
-
