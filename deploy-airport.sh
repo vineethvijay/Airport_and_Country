ebe463b3f707fafd04078b2b2d5411be2639c9c7
Vineeth Vijay <vineethvijay777@gmail.com>
2018-06-21 07:26:31 +0530

    Update deploy-airport.sh

--- deploy-airport.sh
+++ deploy-airport.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+bash clean.sh
 
 mv airport/run.jar airport/airports-assembly-1.0.1.jar
 cp airport/airports-assembly-1.1.0.jar airport/run.jar
-#bash clean.sh
 
 docker-compose up --build
 
